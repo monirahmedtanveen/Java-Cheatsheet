f0587862adf93ef19c83a70e91356bbfa3f92d4c
Monir <monir@fastsolutioninc.com>
2022-03-02 21:25:01 +0600

    inheritence

---
+++ Inheritance/src/com/monirtanveen/Main.java
@@ -0,0 +1,11 @@
+package com.monirtanveen;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Animal animal = new Animal("Animal", 1, 1, 5, 6);
+
+        Dog dog = new Dog("Shiba Inu", 8, 20, 2, 4, 1, 20, "long silky");
+        dog.eat();
+    }
+}
