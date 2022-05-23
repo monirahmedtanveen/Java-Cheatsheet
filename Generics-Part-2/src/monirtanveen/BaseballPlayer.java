56db865dbc9d27a925f3d94bde6d52b1db96d93d
Monir <monir@fastsolutioninc.com>
2022-05-23 06:21:17 +0600

    Generics Part 2

---
+++ Generics-Part-2/src/monirtanveen/BaseballPlayer.java
@@ -0,0 +1,7 @@
+package monirtanveen;
+
+public class BaseballPlayer extends Player {
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
---
+++ Generics-Part-2/src/monirtanveen/FootballPlayer.java
@@ -0,0 +1,7 @@
+package monirtanveen;
+
+public class FootballPlayer extends Player {
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
