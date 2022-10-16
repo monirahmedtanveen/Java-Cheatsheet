47deed025d52c283c2e2d482323bbbaf07160a0f
Monir Ahmed <monir@newroztech.com>
2022-10-16 21:03:26 +0600

    feat: lambda method reference example

---
+++ JavaLambda/LambdaBasic/src/com/monirtanveen/unit3/MethodReferenceExample2.java
@@ -0,0 +1,35 @@
+package com.monirtanveen.unit3;
+
+import com.monirtanveen.unit1.Person;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class MethodReferenceExample2 {
+
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("Charlie", "Dickens", 60),
+                new Person("Lewis", "Carroll", 42),
+                new Person("Thomas", "Carlyle", 51),
+                new Person("Charlotte", "Bronte", 45),
+                new Person("Mattew", "Arnold", 39)
+        );
+
+        System.out.println("Printing all persons");
+        performConditionally(people, person -> true, System.out::println); // p -> method(p)
+
+        Consumer<Person> consumer = System.out::println;
+        consumer.accept(new Person("Monir", "Ahmed", 45));
+    }
+
+    private static void performConditionally(List<Person> people, Predicate<Person> predicate, Consumer<Person> consumer) {
+        for (Person p : people) {
+            if (predicate.test(p)) {
+                consumer.accept(p);
+            }
+        }
+    }
+}
